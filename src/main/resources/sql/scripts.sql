8ca5d6bcb361233230f9320adfdb3782fb59a4e6
Sean Fang <fangzhengonly@gmail.com>
2023-06-25 18:46:53 -0400

    create a custom table for authentication

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -19,3 +19,12 @@ CREATE TABLE `authorities`(
 
 INSERT IGNORE INTO `users` VALUES(NULL, 'happy','12345',1);
 INSERT IGNORE INTO `authorities` VALUES(NULL,'happy','write');
+
+create table `customer`(
+	`id` int not null primary key auto_increment,
+    `email` varchar(48) not null,
+    `pwd` varchar(48) not null,
+    `role` varchar(48) not null
+);
+
+insert into `customer`(`email`,`pwd`,`role`) values ('john@example.com','123','admin');
