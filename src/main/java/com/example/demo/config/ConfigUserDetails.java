74209b32a81cd421a60264c43f51de20b1f74b03
Sean Fang <fangzhengonly@gmail.com>
2023-06-27 18:58:31 -0400

    add custom authentication provider

--- src/main/java/com/example/demo/config/ConfigUserDetails.java
+++ src/main/java/com/example/demo/config/ConfigUserDetails.java
@@ -14,23 +14,25 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class ConfigUserDetails implements UserDetailsService {
-    @Autowired
-    CustomerRepo customerRepo;
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String userName, password = null;
-        List<GrantedAuthority> authorities = null;
-        List<Customer> customers = customerRepo.findByEmail(username);
-        if(customers.size()==0)
-            throw new UsernameNotFoundException("user info not found with the username: "+username);
-        else {
-            userName = customers.get(0).getEmail();
-            password = customers.get(0).getPwd();
-            authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
-        }
-        return new User(userName,password,authorities);
-    }
-}
+//
+//// this user details manager is going to be used by the DaoAuthenticationProvider( default provider)
+//@Service
+//public class ConfigUserDetails implements UserDetailsService {
+//    @Autowired
+//    CustomerRepo customerRepo;
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        String userName, password = null;
+//        List<GrantedAuthority> authorities = null;
+//        List<Customer> customers = customerRepo.findByEmail(username);
+//        if(customers.size()==0)
+//            throw new UsernameNotFoundException("user info not found with the username: "+username);
+//        else {
+//            userName = customers.get(0).getEmail();
+//            password = customers.get(0).getPwd();
+//            authorities = new ArrayList<>();
+//            authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+//        }
+//        return new User(userName,password,authorities);
+//    }
+//}
---
+++ src/main/java/com/example/demo/config/CustomAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private CustomerRepo customerRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepo.findByEmail(username);
+        if(customers.size()>0){
+            if(passwordEncoder.matches(pwd,customers.get(0).getPwd())){
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username,pwd,authorities);
+            }else {
+                throw new BadCredentialsException("invalid password");
+            }
+        }else{
+            throw new BadCredentialsException("no user found");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
